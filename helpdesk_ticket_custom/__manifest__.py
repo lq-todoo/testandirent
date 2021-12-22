0f83f1ffa75d7acae959be9ac61dcc5d073a70e1
jotate70 <42453302+jotate70@users.noreply.github.com>
2021-12-22 12:53:33 -0500

    Update 15.1.4, added planned date required

--- helpdesk_ticket_custom/__manifest__.py
+++ helpdesk_ticket_custom/__manifest__.py
@@ -26,7 +26,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'helpdesk',
-    'version': '15.1.2',
+    'version': '15.1.4',
 
     # any module necessary for this one to work correctly
     'depends': [
