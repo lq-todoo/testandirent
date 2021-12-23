acdfe7e203e17efa41e990f3be23c704522d3591
jotate70 <42453302+jotate70@users.noreply.github.com>
2021-12-23 17:25:34 -0500

    Update 15.2.0 field current location

--- helpdesk_ticket_custom/models/helpdesk_ticket_extended.py
+++ helpdesk_ticket_custom/models/helpdesk_ticket_extended.py
@@ -5,11 +5,13 @@ from odoo import models, fields, api, exceptions
 class helpdesk_ticket_extended(models.Model):
     _inherit = 'helpdesk.ticket'
 
-    x_visibility_related = fields.Boolean(string='Campo oculto', related='team_id.x_visibility', store=True, readonly=True)
+    x_visibility_related = fields.Boolean(string='Campo oculto', related='team_id.x_visibility', store=True,
+                                          readonly=True)
     x_classification = fields.Many2one(comodel_name='helpdesk_classification', string='clasificación')
     x_project = fields.Many2one(comodel_name='helpdesk_project', string='Proyecto', required="True")
     x_family = fields.Many2one(comodel_name='helpdesk_family', string='Familia', required="True")
     x_sub_group = fields.Many2one(comodel_name='helpdesk_sub_group', string='Sub grupo', required="True")
+    current_location = fields.Char(string='Ubicación actual', related='x_project.current_location', store=True)
 
     # Se aplica un decorador que detecta el cambio del campo partner_id
     @api.onchange('partner_id')
