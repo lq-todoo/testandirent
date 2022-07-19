25ad56ab2aadd50b6bd920f7ed3c45fea3618833
Julián Toscano <42453302+jotate70@users.noreply.github.com>
2022-07-19 09:48:51 -0500

    Update

--- helpdesk_ticket_custom/models/project_custom.py
+++ helpdesk_ticket_custom/models/project_custom.py
@@ -10,6 +10,15 @@ class Project(models.Model):
                                           column1='project_project_id', column2='res_partner_id', string='Compañias')
     current_location = fields.Many2one(comodel_name='project_location', string='Ubicación actual',
                                        help='Agregar la ciudad donde se encuentra la sede del cliente')
+    # Concatenar     
+    def name_get(self):
+        result = []
+        for rec in self:
+            name = rec.name + ' [ ' + str(rec.analytic_account_id.code) + ' ]'
+            result.append((rec.id, name))
+        return result
+   
+
 
 
 
